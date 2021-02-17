b395066a929c7854975c2b33e0c2f6d74849a4bf
jakestrouse00 <jakestrouse00@gmail.com>
2021-02-17 09:36:44 -0500

    added more comments

--- tests/multi-thread-blockingTimeout.py
+++ tests/multi-thread-blockingTimeout.py
@@ -4,8 +4,9 @@ import time
 
 
 def use_proxy(manager):
-    # will block until a new proxy is ready or it takes longer than 2 seconds to get a proxy
-    proxy = manager.random(blocking=True, timeout=2)
+    # will block until a new proxy is ready or it takes longer than 1 seconds to get a proxy
+    # (This is made to fail since each proxy will be held for at least 2 seconds before being released)
+    proxy = manager.random(blocking=True, timeout=1)
     if proxy is not None:
         print(f"using proxy: {proxy.address()}\n")
         time.sleep(2)
